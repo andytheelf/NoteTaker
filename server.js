e216fa568b5ba77881687751e0b50632a601d385
andytheelf <ballew22@gmail.com>
2020-05-29 17:26:17 -0500

    port addition

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+//Require const.
+
+const express = require("express");
 const app = express();
+
+//Port add in 
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+//App.listen addition to call port 
+app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+})
