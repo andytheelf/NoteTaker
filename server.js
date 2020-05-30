e032082a5a8e7ce69457e5f8b307779037040afd
andytheelf <ballew22@gmail.com>
2020-05-30 16:54:14 -0500

    port update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 //get request for api route -- view all notes
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 //get request for api route -- view note by id
@@ -31,6 +31,37 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//post request to save notes
+app.post("/api/notes", function(req, res) {
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = notes.length.toString();
+    newNote.id = uniqueID;
+    notes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    console.log("New note saved to database! Content: ", newNote);
+    res.json(notes);
+});
+
+//delete request to delete a saved note
+app.delete("/api/notes/:id", function(req, res) {
+    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Note deleted!`);
+    notes = notes.filter((currNote) => {
+        return currNote.id != noteID;
+    });
+
+    for (currNote of notes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+});
 
 //Starts server
 app.listen(PORT, function() {
